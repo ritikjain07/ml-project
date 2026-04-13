83c3d50300321f81a5b81d115818fb4f6070f013
ritikjain07 <ritikjain4560@gmail.com>
2026-04-13 13:40:26 +0530

    refactor error message formatting and logger path setup

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,18 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error occurred in script: [{0}] line number: [{1}] error message: [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
+    error_message = (
+        "Error occurred in script: [{0}] line number: [{1}] "
+        "error message: [{2}]"
+    ).format(
+        file_name,
+        exc_tb.tb_lineno,
+        str(error),
     )
 
     return error_message
@@ -17,7 +22,7 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
+        logging.error(self.error_message)
 
     def __str__(self):
         return self.error_message
-
